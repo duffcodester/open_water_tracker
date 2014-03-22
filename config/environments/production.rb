a3962fb223b886dec68845d00eb7e1d490065757
Michael Pierce <michael@affektive.com>
2014-03-21 19:52:16 -0600

    Attempt to fix heroku bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Weowe::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(mangle: false)}
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
