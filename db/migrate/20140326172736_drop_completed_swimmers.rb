91a713a614ea025a9fbb10177c5344d66cac46c0
Michael Pierce <michael@affektive.com>
2014-03-27 13:44:31 -0600

    Heroku fix

--- db/migrate/20140326172736_drop_completed_swimmers.rb
+++
@@ -1,9 +0,0 @@
-class DropCompletedSwimmers < ActiveRecord::Migration
-    def up
-      drop_table :completed_swimmers
-    end
-
-    def down
-      raise ActiveRecord::IrreversibleMigration
-    end
-end
