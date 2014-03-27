2eb6cfa1a77719200fc41c3db99ec9057e94734d
Michael Pierce <michael@affektive.com>
2014-03-27 14:01:15 -0600

    Heroku fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Weowe::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(mangle: false)
+  config.assets.js_compressor = false
 
   # config.assets.css_compressor = :sass
 
