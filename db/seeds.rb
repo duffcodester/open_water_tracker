2b2ea02d7b8ac802362cce610b5e15b40e925df1
Josh Duffy <jduffy@mines.edu>
2014-04-12 15:52:04 -0600

    updated seeds file for Brian and Marcia

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,47 @@
 User.create!(first_name: 'Michael',
-                 last_name: 'Pierce',
-                 phone_number: '720-387-9691',
-                 email: 'example@example.com',
-                 password: 'foobar22',
-                 password_confirmation: 'foobar22',
-                 admin: true
-               )
+             last_name: 'Pierce',
+             phone_number: '720-387-9691',
+             email: 'example@example.com',
+             password: 'comsa1',
+             password_confirmation: 'comsa1',
+             admin: true
+             )
 
-Analytic.create!(start_date: '2010-01-01',
-                     end_date: '2014-12-12')
+User.create!(first_name: 'Non-Admin',
+             last_name: 'User',
+             phone_number: '720-387-9691',
+             email: 'user@example.com',
+             password: 'comsa1',
+             password_confirmation: 'comsa1',
+             admin: false
+             )
+
+User.create!(first_name: 'Admin',
+             last_name: 'User',
+             phone_number: '720-387-9691',
+             email: 'admin@example.com',
+             password: 'comsa1',
+             password_confirmation: 'comsa1',
+             admin: true
+             )
+
+User.create!(first_name: 'Brian',
+             last_name: 'Hoyt',
+             phone_number: '111-222-3333',
+             email: 'bkhoyt@gmail.com',
+             password: 'comsa1',
+             password_confirmation: 'comsa1',
+             admin: true
+             )
+
+User.create!(first_name: 'Marcia',
+             last_name: 'Anziano',
+             phone_number: '111-222-3333',
+             email: 'marcia.anziano@gmail.com',
+             password: 'comsa1',
+             password_confirmation: 'comsa1',
+             admin: true
+             )
+
+Analytic.create!(start_date: '2014-01-01',
+                 end_date: '2014-12-31')
