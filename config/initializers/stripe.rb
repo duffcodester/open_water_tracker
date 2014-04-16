df32413e72b24c187fc707bd934a7ca7843878af
Josh Duffy <jduffy@mines.edu>
2014-04-16 09:14:52 -0600

    removed stripe file

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  publishable_key: ENV['pk_test_br3CFmqOcRfScd6aHGqxYCr2'],
-  secret_key: ENV['sk_test_4XxR8NRWLVhWAvwEdQNQWMLi']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
