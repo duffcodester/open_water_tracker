ad9128f672608cd79db6c0da16c13ecfa9abe5db
Michael Pierce <michael@affektive.com>
2014-04-16 19:26:50 -0600

    Fixed query parameters

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,6 @@ class StaticPagesController < ApplicationController
   def analytics
     @start_date = params[:start_date]
     @end_date = params[:end_date]
-    @records = SwimRecord.where(created_at: @start_date..@end_date, completed:true)
+    @records = SwimRecord.where(check_out: @start_date..@end_date, completed:true)
   end
 end
