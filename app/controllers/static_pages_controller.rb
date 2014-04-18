fc17bf0ba44d8b75e25e0c84a13f07bf2b465946
Josh Duffy <jduffy@mines.edu>
2014-04-18 02:25:36 -0600

    fixed analytic query bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def analytics
     @start_date = params[:start_date]
     @end_date = params[:end_date]
-    @records = SwimRecord.where(check_out: "#{@start_date} 00:00:01 UTC".."#{@end_date} 23:59:59 UTC", completed: true)
+    @records = SwimRecord.where(check_out: @start_date..{@end_date}, completed: true)
   end
 end
