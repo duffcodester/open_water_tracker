2f0eb02ac2390f16361b16a73aef1be6eb66d02d
Michael Pierce <michael@affektive.com>
2014-05-03 17:51:59 -0600

    Disabled devise lockable closes #235

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable,
          :rememberable,
-         :lockable,
          :trackable,
          :timeoutable,
          timeout_in: 120.minutes
