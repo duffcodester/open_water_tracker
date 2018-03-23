4c0d3467ba7ba697252f4249b180a48476c83261
duffcodester <josh.m.duffy@gmail.com>
2018-03-23 17:38:06 -0600

    remove comma for swim_records

--- app/models/swim_record.rb
+++ app/models/swim_record.rb
@@ -3,19 +3,4 @@ class SwimRecord < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :swimmer_id, :check_in_user_id, :check_in
-
-  comma do
-    swimmer last_name: 'Last Name'
-    swimmer first_name: 'First Name'
-    swimmer mi: 'Middle Initial'
-    swimmer phone_number: 'Phone Number'
-    check_in 'Check In'
-    # check_in_user_id 'User ID'
-    # check_in_first_name 'First Name'
-    # check_in_last_name 'Last Name'
-    check_out 'Check Out'
-    # check_out_user_id 'User ID'
-    # check_out_first_name 'First Name'
-    # check_out_last_name 'Last Name'
-  end
 end
