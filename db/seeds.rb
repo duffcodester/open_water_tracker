3ff581455e508f5b0ffdadca626070466319a59b
duffcodester <josh.m.duffy@gmail.com>
2019-05-04 10:14:55 -0600

    seed loop fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,27 +3,31 @@ User.destroy_all
 
 Account.create!(name: "Development")
 
-User.create!(first_name: 'Non-Admin',
-             last_name: 'User',
-             phone_number: '720-387-9691',
-             email: 'user@example.com',
-             password: 'openwater',
-             password_confirmation: 'openwater',
-             admin: false,
-             account_id: Account.first.id
-             )
+User.create(first_name: 'Non-Admin',
+            last_name: 'User',
+            phone_number: '720-387-9691',
+            email: 'user@example.com',
+            password: 'openwater',
+            password_confirmation: 'openwater',
+            admin: false,
+            account_id: Account.first.id
+            )
 
-User.create!(first_name: 'Admin',
-             last_name: 'User',
-             phone_number: '720-387-9691',
-             email: 'admin@example.com',
-             password: 'openwater',
-             password_confirmation: 'openwater',
-             admin: true,
-             account_id: Account.first.id
-             )
+User.create(first_name: 'Admin',
+            last_name: 'User',
+            phone_number: '720-387-9691',
+            email: 'admin@example.com',
+            password: 'openwater',
+            password_confirmation: 'openwater',
+            admin: true,
+            account_id: Account.first.id
+            )
 
-100.times.do {
+100.times do
 
-  Swimmer.create(first)
-}
+  Swimmer.create(first_name: Faker::Name.first_name,
+                 last_name: Faker::Name.last_name,
+                 phone_number: Faker::PhoneNumber.phone_number,
+                 account_id: Account.first.id)
+  
+end
