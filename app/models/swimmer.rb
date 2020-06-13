bd6e69e571398978c55dfd5df3d115121666e69b
duffcodester <josh.m.duffy@gmail.com>
2020-06-13 14:13:35 -0600

    add additional fields to swimmer import

--- app/models/swimmer.rb
+++ app/models/swimmer.rb
@@ -17,16 +17,16 @@ class Swimmer < ActiveRecord::Base
 
       (2..spreadsheet.last_row).each do |i|
         @row = Hash[[header, spreadsheet.row(i)].transpose]
-        @swimmer = Swimmer.find_by(first_name: @row['first_name'], last_name: @row['last_name'], account_id: @account_id, deleted_at: nil)
+        
+        @swimmer = Swimmer.find_by(
+          first_name: @row['first_name'], 
+          last_name: @row['last_name'], 
+          account_id: @account_id,
+          deleted_at: nil
+        )
 
         if @swimmer # existing
           if @swimmer.update(user_hash)
-            if @swimmer.phone_number == ''
-              @swimmer.phone_added = false
-            elsif @swimmer.phone_number
-              @swimmer.phone_added = true
-            end
-            @swimmer.save!
           	puts "updated: " + @swimmer.reload.to_s
           else
             puts "update error" + @swimmer.to_s
@@ -35,12 +35,6 @@ class Swimmer < ActiveRecord::Base
           @swimmer = Swimmer.new(user_hash)
 
           if @swimmer.save
-            if @swimmer.phone_number == ''
-              @swimmer.phone_added = false
-            elsif @swimmer.phone_number
-              @swimmer.phone_added = true
-            end
-            @swimmer.save!
             puts "created" + @swimmer.reload.to_s
           else
             puts "create error:" + @swimmer.to_s
@@ -64,7 +58,12 @@ class Swimmer < ActiveRecord::Base
       first_name: @row['first_name'].to_s.squish,
       last_name: @row['last_name'].to_s.squish,
       phone_number: @row['phone_number'].to_s.squish,
-      account_id: @account_id
+      account_id: @account_id,
+      email: @row['email'].to_s.squish.downcase,
+      date_of_birth: Time.zone.parse(@row['date_of_birth'].to_s),
+      emergency_first: @row['emergency_contact_first_name'].to_s.squish,
+      emergency_last: @row['emergency_contact_last_name'].to_s.squish,
+      emergency_phone: @row['emergency_contact_phone_number'].to_s.squish
     }
     # :nocov:
   end
